738fcc822aee5b2fc9b0249b986381c162ef5d20
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-18 04:34:32 +0800

    Revert "取消hack补丁"
    This reverts commit d99067330043449d5f5bcf3a2201e30924dc6d09.

--- devices/common/diy-part1.sh
+++ devices/common/diy-part1.sh
@@ -19,7 +19,7 @@ echo 'src-git zhuxiaole https://github.com/zhuxiaole/my-packages-for-x-wrt' >>fe
 rm -rf package/network/config/firewall
 git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/immortalwrt" "package/network/config/firewall_pkg" package/network/config/firewall
 git clone --depth 1 -b dev https://github.com/llccd/openwrt-fullconenat package/network/utils/fullconenat
-#git_sparse_clone openwrt-18.06 "https://github.com/immortalwrt/immortalwrt" "target/linux/generic/hack-4.19/patch_952" target/linux/generic/hack-4.19/952-net-conntrack-events-support-multiple-registrant.patch
-#git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.10/patch_952" target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
-#git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.15/patch_952" target/linux/generic/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch
-#git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.4/patch_952" target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+git_sparse_clone openwrt-18.06 "https://github.com/immortalwrt/immortalwrt" "target/linux/generic/hack-4.19/patch_952" target/linux/generic/hack-4.19/952-net-conntrack-events-support-multiple-registrant.patch
+git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.10/patch_952" target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch
+git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.15/patch_952" target/linux/generic/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch
+git_sparse_clone master "https://github.com/coolsnowwolf/lede" "target/linux/generic/hack-5.4/patch_952" target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
