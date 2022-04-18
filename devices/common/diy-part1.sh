92a21ff0d03a887e6a19e99726eeaeb8f678f28a
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-18 20:44:06 +0800

    替换com.x-wrt源

--- devices/common/diy-part1.sh
+++ devices/common/diy-part1.sh
@@ -13,4 +13,5 @@ rm -rf $localdir
 
 sed -i 's|https://github.com/x-wrt/packages.git|https://github.com/zhuxiaole/x-wrt-packages.git|' feeds.conf.default
 sed -i 's|https://github.com/x-wrt/luci.git|https://github.com/zhuxiaole/x-wrt-luci.git|' feeds.conf.default
+sed -i 's|https://github.com/x-wrt/com.x-wrt.git|https://github.com/zhuxiaole/x-wrt-com.git|' feeds.conf.default
 echo 'src-git zhuxiaole https://github.com/zhuxiaole/my-packages-for-x-wrt' >>feeds.conf.default
