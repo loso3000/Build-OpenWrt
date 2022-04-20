8902891aa849a6e9d3539dae0d3eb7bdf88dfca6
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-20 17:15:37 +0800

    修改

--- devices/common/diy-part1.sh
+++ devices/common/diy-part1.sh
@@ -10,8 +10,3 @@ mv -n $@ ../
 cd $GITHUB_WORKSPACE/openwrt
 rm -rf $localdir
 }
-
-sed -i 's|https://github.com/x-wrt/packages.git|https://github.com/zhuxiaole/x-wrt-packages.git|' feeds.conf.default
-sed -i 's|https://github.com/x-wrt/luci.git|https://github.com/zhuxiaole/x-wrt-luci.git|' feeds.conf.default
-sed -i 's|https://github.com/x-wrt/com.x-wrt.git|https://github.com/zhuxiaole/x-wrt-com.git|' feeds.conf.default
-echo 'src-git zhuxiaole https://github.com/zhuxiaole/my-packages-for-x-wrt' >>feeds.conf.default
