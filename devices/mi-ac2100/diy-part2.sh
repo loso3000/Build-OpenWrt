e0cbd6e814d4848d6dffe1691016b8a22902dc83
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-21 14:23:33 +0800

    修改ip

--- devices/mi-ac2100/diy-part2.sh
+++ devices/mi-ac2100/diy-part2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Modify default IP
-default_ip='192.168.31.1'
-sed -i 's/192.168.15.1/$default_ip/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.15.1/$default_ip/g' package/feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
+sed -i 's/192.168.15.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.15.1/192.168.31.1/g' package/feeds/luci/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
--- devices/x86_64/diy-part2.sh
+++ devices/x86_64/diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Modify default IP
-default_ip='10.11.12.2'
-sed -i 's/192.168.15.1/$default_ip/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.15.1/$default_ip/g' package/feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
+sed -i 's/192.168.15.1/10.11.12.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168./10.11./g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.15.1/10.11.12.2/g' package/feeds/luci/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
