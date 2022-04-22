0a5e11bde8262458e3f347ec381324bcfa2630f7
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-23 04:26:58 +0800

    修改mytarget

--- xwrt/devices/common/diy-part1.sh
+++ xwrt/devices/common/diy-part1.sh
@@ -88,7 +88,7 @@ sed -i "/DISTRIB_GITHUB/a\echo \"DISTRIB_GITHUB=\'https://github.com/zhuxiaole/B
 sed -i "/\$DISTRIB_ID/a\sed -i '/DISTRIB_VERSIONS/d' /etc/openwrt_release" feeds/x/base-config-setting/files/uci.defaults
 sed -i "/DISTRIB_VERSIONS/a\echo \"DISTRIB_VERSIONS=\'${REPO_BRANCH}\'\" >> /etc/openwrt_release" feeds/x/base-config-setting/files/uci.defaults
 sed -i "/\$DISTRIB_ID/a\sed -i '/DISTRIB_MY_TARGET/d' /etc/openwrt_release" feeds/x/base-config-setting/files/uci.defaults
-sed -i "/DISTRIB_MY_TARGET/a\echo \"DISTRIB_MY_TARGET=\'${MY_TARGET}\'\" >> /etc/openwrt_release" feeds/x/base-config-setting/files/uci.defaults
+sed -i "/DISTRIB_MY_TARGET/a\echo \"DISTRIB_MY_TARGET=\'${REPO_MY_TARGET}\'\" >> /etc/openwrt_release" feeds/x/base-config-setting/files/uci.defaults
 
 rm -rf feeds/x/luci-app-wizard
 git_sparse_clone master "https://github.com/kiddin9/openwrt-packages" "feeds/x/wizard_luci" luci-app-wizard
