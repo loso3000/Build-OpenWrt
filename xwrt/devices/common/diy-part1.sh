089cd35f0922be87f78ee11bf92c14fe6bb3bdeb
猪小乐 <zhuxiaole@zhuxiaole.org>
2022-04-26 18:03:49 +0800

    修改adguardhome和smartdns

--- xwrt/devices/common/diy-part1.sh
+++ xwrt/devices/common/diy-part1.sh
@@ -12,10 +12,10 @@ rm -rf $localdir
 }
 
 rm -rf feeds/packages/net/smartdns
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/packages" "feeds/packages/net/smartdns_pkg" net/smartdns
+git_sparse_clone main "https://github.com/kenzok8/jell" "feeds/packages/net/smartdns_pkg" smartdns
 
 rm -rf feeds/packages/net/adguardhome
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/packages" "feeds/packages/net/adguardhome_pkg" net/adguardhome
+git_sparse_clone main "https://github.com/kenzok8/small-package" "feeds/packages/net/adguardhome_pkg" adguardhome
 
 rm -rf feeds/packages/net/frp
 git clone --depth 1 https://github.com/kuoruan/openwrt-frp feeds/packages/net/frp
@@ -39,7 +39,7 @@ rm -rf feeds/luci/applications/luci-app-airplay2
 git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "feeds/luci/applications/airplay2_luci" applications/luci-app-airplay2
 
 rm -rf feeds/luci/applications/luci-app-smartdns
-git_sparse_clone openwrt-21.02 "https://github.com/immortalwrt/luci" "feeds/luci/applications/smartdns_luci" applications/luci-app-smartdns
+git_sparse_clone main "https://github.com/kenzok8/jell" "feeds/luci/applications/smartdns_luci" luci-app-smartdns
 
 rm -rf feeds/luci/applications/luci-app-frpc
 git_sparse_clone master "https://github.com/kiddin9/openwrt-packages" "feeds/luci/applications/frpc_luci" luci-app-frpc
