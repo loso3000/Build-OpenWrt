87800fece7629b6046f6f15a4f89f4ffa5143d93
猪小乐 <1365441936@qq.com>
2022-04-30 03:56:33 +0800

    update feeds config

---
+++ xwrt/devices/common/diy-feeds.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'src-git zhuxiaole https://github.com/zhuxiaole/OpenWrt-Packages;x-wrt' >>feeds.conf.default
+echo 'src-git onlyme https://github.com/zhuxiaole/my-packages;master' >>feeds.conf.default
