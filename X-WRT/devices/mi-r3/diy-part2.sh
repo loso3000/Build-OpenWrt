51609c63d92351b7909254081a2736d096a681bb
loso3000 <51484662+loso3000@users.noreply.github.com>
2022-08-26 03:58:46 +0800

    r3

---
+++ X-WRT/devices/mi-r3/diy-part2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Modify default IP
+sed -i 's/192.168.15.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.15.1/192.168.31.1/g' package/feeds/luci/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
